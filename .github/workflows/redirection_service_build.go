fd3f921f1c20391c10fc8151f59f8739614f1b36
Wissem Belguidoum <5852356+elbombardi@users.noreply.github.com>
2023-08-21 10:46:25 +0100

    Create redirection_service_build.go

---
+++ .github/workflows/redirection_service_build.go
@@ -0,0 +1,31 @@
+# This workflow will build a golang project
+# For more information see: https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-go
+
+name: redirection_service_build
+
+on:
+  push:
+    branches: [ "main" ]
+  pull_request:
+    branches: [ "main" ]
+
+jobs:
+
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v3
+
+    - name: Set up Go
+      uses: actions/setup-go@v4
+      with:
+        go-version: '1.21'
+
+    - name: Build
+      run: go build -v ./src/redirection_service/...
+
+    - name: Test
+      run: go test -v --coverprofile redirection_service_cover.out ./src/api_service/...
+    
+    - name: Coverage
+      run: go tool cover --func=redirection_service_cover.out
